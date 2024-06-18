47ba7adad7375ac6b67db159621e3db05762f683
Mr. Jay Patel <jayvekariya2003@gmail.com>
2024-06-18 11:56:42 +0530

    Node.js Basic - import, export files

---
+++ Global.js
@@ -0,0 +1,14 @@
+//Global Object...
+// console.log(global);
+
+// setTimeout(()=>{
+//     console.log("Hello World timeout ....");
+//     clearInterval(int);
+// }, 5000);
+
+// const int = setInterval(()=>{
+//     console.log("Hello World");
+// }, 1000);
+
+// console.log(__dirname);
+// console.log(__filename);
---
+++ People.js
@@ -0,0 +1,6 @@
+const people = ["abc", "buv", "bhf", "hbc"];
+const ages = [23, 26, 28, 23, 17]
+
+console.log(people);
+
+module.exports = {people, ages};
