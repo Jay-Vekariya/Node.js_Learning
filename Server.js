b16e89a824bdcc1517c0522b6e7a42e2bc75ab90
Mr. Jay Patel <jayvekariya2003@gmail.com>
2024-06-19 14:18:08 +0530

    Node.js - Redirects

--- Server.js
+++ Server.js
@@ -23,6 +23,11 @@ const server = http.createServer((req, res)=>{
             path += "about.html";
             res.statusCode = 200;
             break;
+        case "/about-me":
+            res.statusCode = 301;  //moved on
+            res.setHeader("Location", "/about");
+            res.end();
+            break;
         default:
             path += "404P.html";
             res.statusCode = 404;
