acc860caaffac603d5215562cc808737d11295e1
Mr. Jay Patel <jayvekariya2003@gmail.com>
2024-06-19 11:45:15 +0530

    Node.js - Returning html pages

--- Servers/Server.js
+++ Servers/Server.js
@@ -1,14 +1,27 @@
-const http = require("http");
+const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res)=>{
-    console.log(req.url, req.method);
+    // console.log(req.url, req.method);
 
-    //set header to the browser..
+    //set header content type..
     res.setHeader("Content-Type", "text/html");
-    res.write('<head><link rel="styleseet" href="#" ></head>');
-    res.write("<p>Hello World</p>");
-    res.write("<p>Hello agin.. World</p>");
-    res.end();
+    // res.write('<head><link rel="styleseet" href="#" ></head>');
+    // res.write("<p>Hello World</p>");
+    // res.write("<p>Hello agin.. World</p>");
+    // res.end();
+
+    //sent an html file
+    fs.readFile("./index.html", (err, data)=>{
+        if(err){
+            console.log(err.message);
+            res.end();
+        } else{
+            // res.write(data);
+            res.end(data);
+        }
+    });
+
 });
 
 server.listen(3000, "localhost", ()=>{
