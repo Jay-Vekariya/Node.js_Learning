404f1278be69a08a2a9c9c6101d3fb612d65e495
Mr. Jay Patel <jayvekariya2003@gmail.com>
2024-06-19 11:20:52 +0530

    Node.js - Reaponse Object

--- Servers/Server.js
+++ Servers/Server.js
@@ -1,7 +1,14 @@
 const http = require("http");
 
 const server = http.createServer((req, res)=>{
-    console.log("request sent")
+    console.log(req.url, req.method);
+
+    //set header to the browser..
+    res.setHeader("Content-Type", "text/html");
+    res.write('<head><link rel="styleseet" href="#" ></head>');
+    res.write("<p>Hello World</p>");
+    res.write("<p>Hello agin.. World</p>");
+    res.end();
 });
 
 server.listen(3000, "localhost", ()=>{
