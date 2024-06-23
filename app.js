91e30e84645789b27f56daf293a0192cd89dd1b3
Mr. Jay Patel <jayvekariya2003@gmail.com>
2024-06-23 11:47:43 +0530

    ejs : passing data into views.

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 //express app
 const app = express();
+
+//register view engine
+app.set("view engine", "ejs"); 
+
 //port num
 const port = 4000;
 
@@ -11,23 +15,36 @@ app.listen(port, ()=>{
 });
 //Routes
 app.get('/', (req, res)=>{
-    // res.send("<h1>Home Page</h1>");
-    res.sendFile("./Views/index.html", {root: __dirname});
+    const blogs = [
+        {title: "Yoshi finds eggs", snippet: "Lorem ipsum dolor sit amet consectetur"},
+        {title: "Mario finds stars", snippet: "Lorem ipsum dolor sit amet consectetur"},
+        {title: "How to defeat bowser", snippet: "Lorem ipsum dolor sit amet consectetur"}
+    ];
+    // res.send("<h1>Home Page</h1>"); 
+    // res.sendFile("./Views/index.html", {root: __dirname});
+    res.render("index", {title:"Jay", blogs}); 
+    //The res.render() method in JavaScript is typically used in web development frameworks like Express.js. It is responsible for rendering a specified view template and sending the rendered HTML back to the client.
 });
 
 app.get('/about', (req, res)=>{
     // res.send("<h1>About Page</h1>");
-    res.sendFile("./Views/about.html", {root: __dirname});
-
+    // res.sendFile("./Views/about.html", {root: __dirname});
+    res.render("about");
 });
+
+app.get("/about/create", (req, res)=>{
+    res.render("create");
+
+})
 
 //redirect
-app.get("/about-us", (req, res)=>{
-    res.redirect("./about");
-});
+// app.get("/about-us", (req, res)=>{
+//     res.redirect("./about");
+// });
 
 //404 page
 app.use((req, res)=>{
     // res.status(404).send("<h1>404 Page
-    res.status(404),sendFile("./Views/404P.html", {root: __dirname});
+    // res.status(404),sendFile("./Views/404P.html", {root: __dirname});
+    res.status(404).render("404P")
 })
